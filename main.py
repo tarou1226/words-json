71a9930678193b9bab5970576c0865bd1dba4928
tarou <teroro1226@outlook.jp>
2022-09-16 06:57:00 +0000

    add clera func

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 import json
 import ndjson
 import os
-
+import subprocess
 
 #def usage():
 #    # Linuxのようなhelpを作成する。
 #    print("add command:python main.py add [tango] [description]")
 #    print("show command:python main.py show [tango]")
 
-JSON_FALE_PASS = "lists.json"
-NDJSON_FALE_PASS = "lists.ndjson"
+JSON_FALE_PATH = "lists.json"
+NDJSON_FALE_PATH = "lists.ndjson"
 
 def add():
 
@@ -23,7 +23,7 @@ def add():
             continue
         # 単語が数字のみの場合、処理を戻す
         elif(word.isdigit() == True):
-            print("数字のみで構成されています")
+            print("単語が数字のみで構成されています")
             print("やり直してください\n")
             continue
         else:
@@ -32,7 +32,7 @@ def add():
     word_dict = {word: description}
 
     if(os.path.exists("lists.ndjson") == True):
-        print("ファイルが存在します")
+        #print("ファイルが存在します")
         with open("lists.ndjson", 'a') as add_ndjson:
             writer = ndjson.writer(add_ndjson)
             writer.writerow(word_dict)
@@ -43,7 +43,7 @@ def add():
             write_json.write(str(data).replace("'", '"'))
 
     else:
-        print("ファイルが存在しません")
+        #print("ファイルが存在しません")
         with open("lists.ndjson", mode = "w") as write_ndjson:
             writer = ndjson.writer(write_ndjson)
             writer.writerow(word_dict)
@@ -52,6 +52,8 @@ def add():
         data = json.dumps(data, indent = 4, ensure_ascii = False)
         with open("lists.json", "w") as write_json:
             write_json.write(str(data).replace("'", '"'))
+    
+    print("add処理が終了しました")
 
 def show():
     word = input("単語:")
@@ -59,17 +61,29 @@ def show():
         show_data = json.load(read_json)
     show_data = list(show_data)
     word_dec = [i.get("{}".format(word)) for i in show_data]
-    print(*list(filter(lambda item: item != None, word_dec)))
+    word_dec_search = list(filter(lambda item: item != None, word_dec))
+    if word_dec_search:
+        print(*word_dec_search)
+    else:
+        print("wordが存在していません")
+        print("wordを登録してください")
 
-    #print("show func")
+    print("show処理が終了しました")
 
+def clear():
+    """
+    cmd = input("word or all")
+    if(cmd == "word"):
+        word = input("word:")
+
+    elif(cmd == "all"):
+    """
+    subprocess.run(["rm {}".format(JSON_FALE_PATH)], encoding = "utf-8", shell = True)
+    subprocess.run(["rm {}".format(NDJSON_FALE_PATH)], encoding = "utf-8", shell = True)
+    print("clear処理が終了しました")
 
 def main():
-    # コマンドがない場合の処理
-    #if(len(sys.argv) < 2):
-    #    usage()
-    #    sys.exit()
-    # sys.argv[index] = ["main.py", "add/show"]
+    # sys.argv[index] = ["main.py", "add/show/clear"]
     try:
         cmd = sys.argv[1]
 
@@ -77,6 +91,8 @@ def main():
             add()
         elif(cmd == "show"):
             show()
+        elif(cmd == "clear"):
+            clear()
         else:
             usage()
             sys.exit()  
